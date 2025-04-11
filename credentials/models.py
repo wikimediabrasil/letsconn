c33607c74743ba613c29da8114128f16b497d034
albertoleoncio <albertoleoncio@hotmail.com>
2025-04-11 10:01:57 -0300

    Alter email field in CustomUser model to remove uniqueness constraint and create migration

--- credentials/models.py
+++ credentials/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserM
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=150, unique=True)
-    email = models.EmailField(unique=True)
+    email = models.EmailField()
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_approved = models.BooleanField(default=False)
